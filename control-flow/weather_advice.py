209cd6a9072fdc7a8f7a5faff348a6924790c424
Laura Neema <laurajuma52@gmail.com>
2025-11-16 15:31:40 +0300

    Fix input prompt for ALX checker

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,15 +1,16 @@
-print('Welcome to weather advice programme:)')
-weather = input(" What's the weather like today? (sunny/rainy/cold):.")
-weather=weather.lower()
+print("Welcome to the Weather Recommendation Program!")
 
-if weather == 'sunny':
-  print('Wear a t-shirt and sunglasses.')
-elif weather == 'rainy':
-  print("Don't forget your umbrella and a raincoat.")
-elif weather == 'cloudy':
-  print("Make sure to wear a warm coat and a scarf.")
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-  print("Sorry, I don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
+
 
 
 
