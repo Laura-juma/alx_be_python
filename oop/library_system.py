3f3cf484847f879e6c10d2654c8509e265bb0039
Laura Neema <laurajuma52@gmail.com>
2025-12-06 20:41:34 +0300

    fix bugs for the checker

--- oop/library_system.py
+++ oop/library_system.py
@@ -23,13 +23,14 @@ class PrintBook(Book):
     return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
-  books = []
+   def __init__(self):
+        self.books = []
 
+   def add_book(self, book):
+        self.books.append(book)
 
-  def add_book(self, book):
-    Library.books.append(book)
-
-  def list_books(self):
-    for book in Library.books:
-      print(book) 
+   def list_books(self):
+        for book in self.books:
+            print(book)
+ 
   
